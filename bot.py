814d9394f79450c9d1ea347d5c504edcae4ddd7f
egor5q <31898567+egor5q@users.noreply.github.com>
2020-07-14 16:21:35 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -14,13 +14,64 @@ bot = telebot.TeleBot(token)
 
 
 client=MongoClient(os.environ['database'])
-db=client.
+db=client.samosbor
 users=db.users
+codes = db.codes
+if codes.find_one({}) == None:
+    codes.insert_one({'code':0})
 
 
 def medit(message_text,chat_id, message_id,reply_markup=None,parse_mode=None):
     return bot.edit_message_text(chat_id=chat_id,message_id=message_id,text=message_text,reply_markup=reply_markup,
                                  parse_mode=parse_mode)   
+
+def code():
+    c = codes.find_one({})['code']
+    codes.update_one({},{'$inc':{'code':1}})
+    return str(c)
+
+def createuser(user):
+    return {
+        'id':user.id,
+        'units':{},
+        'current_unit':None
+    }
+
+def createunit(user):
+    c = code()
+    return {c:{
+        'code':c,
+        'id':user.id,
+        'name':None,
+        'show_name':None,
+        'inventory':{},
+        'location':None,
+        'stats':{
+            'maxhunger':100,
+            'hungher':100,
+            
+            'maxhealth':100,
+            'health':100,
+            
+            'maxwater':100,
+            'water':100,
+            
+            'maxrest':100,
+            'rest':100,
+            
+            'maxsleep':100,
+            'sleep':100,
+            
+            'strength':10,
+            'agility':10,
+            'intelligence':10,
+            'stamina':10,
+            
+            'maxweight':100
+        }
+    }
+           }
+
 
 print('7777')
 bot.polling(none_stop=True,timeout=600)
